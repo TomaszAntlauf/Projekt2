74e7ae4a60bb1ecc489f7429cee7f349effeed89
tomaant234 <tomaant234@student.polsl.pl>
2019-11-12 15:08:17 +0100

    projekt 5 5

--- src/main/java/com/example/demo/StudentSerwis.java
+++ src/main/java/com/example/demo/StudentSerwis.java
@@ -1,10 +1,11 @@
 package com.example.demo;
 
 //import java.util.List;
+import com.example.demo.db.StudentRepository;
 import io.vavr.collection.List;
 
 public class StudentSerwis {
-    private List<Student> students = List.empty();
+   /* private List<Student> students = List.empty();
     List<Student> getStudents() {
         return this.students;
     }
@@ -15,4 +16,18 @@ public class StudentSerwis {
         return created;
 
     }
+    */
+   private final StudentRepository repository;
+    public StudentSerwis(StudentRepository repository) {
+        this.repository = repository;
+    }
+
+    List<Student> getStudents() {
+        throw new UnsupportedOperationException();
+    }
+    Student addStudent(final NewStudent newStudent) {
+        throw new UnsupportedOperationException();
+    }
+
+
 }
